b2bd3bcc7128f00ee1937b78ea7845df62a127ec
Dhanesh Desai <dhanesh.d.desai@gmail.com>
2018-10-06 18:15:16 -0400

    New file

---
+++ untitled.js
@@ -0,0 +1,24 @@
+var database = [
+	{
+		username: "Dhanesh",
+		password: "supersecret"
+	}
+
+];
+
+var newsFeed = [
+
+	{
+		username: "Bobby",
+		timeline: "So tired from all that learning"
+	},
+
+	{
+		username: "Sally",
+		timeline: "YOLO"
+	}
+
+];
+
+var userNamePrompt = prompt("What is your username?");
+var passwordPrompt = prompt("What is your password?");
